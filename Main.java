64261f3841ae884e13f330203110dde5e7a37707
baogh31 <96731163+baogh31@users.noreply.github.com>
2022-06-13 15:09:52 +0700

    Hiển thị lời chào Ss1

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập tên ");
+        String name = scanner.nextLine();
+        System.out.println("Chào " + name);
+    }
+}
