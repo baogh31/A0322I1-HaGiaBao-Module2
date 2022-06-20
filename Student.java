a444573555cd64b24bd4890c10b79f8697e43db9
baogh31 <96731163+baogh31@users.noreply.github.com>
2022-06-20 21:05:20 +0700

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+
+        System.out.println(student.setName("Bao"));
+        System.out.println(student.setClasses("A03"));
+    }
+
+}
