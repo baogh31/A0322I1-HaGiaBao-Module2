fc1356a810ec80c0692935b99ef7a8da5ce1962f
baogh31 <96731163+baogh31@users.noreply.github.com>
2022-06-20 21:02:36 +0700

    Create TestCircle.java

---
+++ TestCircle.java
@@ -0,0 +1,8 @@
+package com.company;
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle=new Circle();
+        System.out.println("radius: "+circle.getRadius());
+        System.out.println("area: "+ circle.getArea());
+    }
+}
